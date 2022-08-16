e60409f2b7bf3f551ffaf3c6d126c958c612fae4
Thiago Trindade <thiagotrindade628@gmail.com>
2022-08-16 01:39:44 -0300

    refactore: "style and tailwind"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,15 @@
 module.exports = {
   content: ["./src/**.{html, js}"],
   theme: {
-    extend: {},
-    screens: {
-      'mobile': '375',
-      // => @media (min-width: 640px) { ... }
-
-      'tablet': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'desktop': '1440px',
-      // => @media (min-width: 1280px) { ... }
-    }
+    extend: {
+      purge: { 
+        enabled: true, content: [ './src/**.html' ] 
+      },
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      }
+    },
   },
   plugins: [],
 }
